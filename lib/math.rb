9d4b48789ef6ef631a38cc7e41bd7d0180feeae3
Elijah Florence <efloren@siue.edu>
2019-03-28 18:07:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,11 +15,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num1%num2
+  return num1%num2
 end
 
 def square_root(num)
-  Math.sqrt(num)
+  return Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
